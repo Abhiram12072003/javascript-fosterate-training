621d072356a46f09ba80c9df50469ff48d931405
Abhiram12072003 <kilaruabhiram2003@gmail.com>
2022-03-22 00:04:42 +0530

    Worked on operators;

---
+++ operator.js
@@ -0,0 +1,15 @@
+console.log("Hry")
+var a=2,b=3;
+var sum=a+b;
+console.log(a,' + ',b,' = ', sum)   
+var diff=a-b;
+console.log(a,' - ',b,' = ', diff) 
+var mul=a*b;
+console.log(a,' * ',b,' = ', mul) 
+var quotient=a/b;
+console.log(a,' / ',b,' = ', quotient)
+var power=a**b;
+console.log(a,' ** ',b,' = ', power) 
+//We can add string and number also
+var x="hey"+12
+console.log(x)
